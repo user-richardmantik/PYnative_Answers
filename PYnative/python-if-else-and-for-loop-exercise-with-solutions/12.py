a95ffcc2aed8c12943aad44a314942b1ee749cc5
user-richardmantik <143813163+user-richardmantik@users.noreply.github.com>
2023-12-09 22:17:15 +0700

    Update 12.py

--- PYnative/python-if-else-and-for-loop-exercise-with-solutions/12.py
+++ PYnative/python-if-else-and-for-loop-exercise-with-solutions/12.py
@@ -4,5 +4,11 @@ fibonacci_list = [0, 1]
 for i in range (2, COUNT_NUMS):
     fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
 
-fibonacci_list = [str(s) for s in fibonacci_list]
-print (' '.join(fibonacci_list))
+
+if (COUNT_NUMS > 1):
+    fibonacci_list = [str(s) for s in fibonacci_list]
+    print (' '.join(fibonacci_list))
+elif (COUNT_NUMS == 1):
+    print (0)
+elif (COUNT_NUMS == 0):
+    print ()
