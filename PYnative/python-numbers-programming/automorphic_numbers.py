0c12288d073620a65a99c3dba53219a17f9f9c00
user-richardmantik <143813163+user-richardmantik@users.noreply.github.com>
2023-12-12 08:26:07 +0700

    Create automorphic_numbers.py

---
+++ PYnative/python-numbers-programming/automorphic_numbers.py
@@ -0,0 +1,70 @@
+import math
+
+def automorphic_numbers(power):
+    automorphic_numbers = list()
+
+    top = 0
+    n = 5
+    base = 10
+    k = 1
+    for i in range(1, power+1):
+        size = 2**i
+        n = (3*n**2 - 2*n**3) % (base**size)
+        for j in range(k, size+1):
+            m5 = n % (base**j)
+            m6 = base**j + 1 - m5
+            _, middle, bigger  = sorted([top, m5, m6])
+            if middle > top:
+                automorphic_numbers.append(middle)
+            automorphic_numbers.append(bigger)
+            top = bigger
+        k = size + 1
+
+    return automorphic_numbers
+
+automorphic_numbers = automorphic_numbers(4)
+
+print (', '.join([str(num) for num in automorphic_numbers]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def is_automorphic(num):
+    last_digit  = num % 10
+
+    if (last_digit >= 5 and last_digit <= 6 and num > 1):
+        num_squared     = num ** 2
+    
+        return (num_squared - num) % (10 ** count_digits(num)) == 0
+    elif (num <=1):
+        return True
+    else:
+        return False
+
+def count_digits(num): 
+    return math.floor(math.log10(num)+1) 
+
+start_num   = 0
+end_num     = 87109376
