4becaa79aac139a974add86e6aa8db0c9df43294
user-richardmantik <143813163+user-richardmantik@users.noreply.github.com>
2023-12-13 07:50:48 +0700

    Update is_prime.py

--- PYnative/python-numbers-programming/is_prime.py
+++ PYnative/python-numbers-programming/is_prime.py
@@ -2,7 +2,18 @@ import time
 import math
 
 start_time = time.time()
-num = 116674323278833
+num = 1029384567
+
+import math
+
+def get_divisors_list(num):
+    divisors_list = list()
+    if (num != 1):
+        for num_i in range (1, math.ceil(num/2)+1):
+            if (num % num_i == 0):
+                divisors_list.append(num_i)
+    
+    return divisors_list
 
 def is_prime(num):
     for num_1 in range (2, math.floor(math.sqrt(num)) + 1):
@@ -11,6 +22,8 @@ def is_prime(num):
     return num != 1 and True
 
 print (num, 'is ' + ('NOT ' if (not is_prime(num)) else '') + 'a prime number')
+if (not is_prime(num)):
+    print ('divisors =', get_divisors_list(num))
 
 end_time = time.time()
 print("The time of execution of above program is :", (end_time-start_time) * 10**3, "ms")
