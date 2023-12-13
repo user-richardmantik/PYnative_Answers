d0c3450c356a6db3b4d0e69b6bb4030cede4c682
user-richardmantik <143813163+user-richardmantik@users.noreply.github.com>
2023-12-14 04:44:32 +0700

    Update empty_string.py

---
+++ PYnative/python_q_and_a/empty_string.py
@@ -0,0 +1,7 @@
+# empty string ('') for joining
+numbers_list     = ['1','2','3','4','5']
+numbers_list_str = ''.join(numbers_list)
+print(numbers_list_str)
+
+#empty string ('') for printing without newline
+print(numbers_list_str, end='')
