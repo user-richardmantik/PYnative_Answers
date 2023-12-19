67a1646229f511d2d035bf0134a47229562e7f5e
user-richardmantik <143813163+user-richardmantik@users.noreply.github.com>
2023-12-20 00:09:48 +0700

    s

--- PYnative/python-numbers-programming/padovan_numbers.py
+++ PYnative/python-numbers-programming/padovan_numbers.py
@@ -1,6 +1,6 @@
 COUNT_NUMS = 15
 
-pedovan_nums = [1, 1, 1]
+padovan_nums = [1, 1, 1]
 
 curr_p = 1
 prev_p = 1
@@ -9,10 +9,10 @@ prev_prev_p = 1
 for n in range(4, COUNT_NUMS + 1):
     next_p = prev_p + prev_prev_p
 
-    pedovan_nums.append(next_p)
+    padovan_nums.append(next_p)
 
     prev_prev_p = prev_p
     prev_p = curr_p
     curr_p = next_p
 
-print(pedovan_nums)
+print(padovan_nums)
