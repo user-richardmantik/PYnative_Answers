e7349b3152b20a7dde070250b03fa452e29f6800
user-richardmantik <143813163+user-richardmantik@users.noreply.github.com>
2023-12-20 22:11:28 +0700

    Update sylvesters_numbers.py

--- PYnative/python-numbers-programming/sylvesters_numbers.py
+++ PYnative/python-numbers-programming/sylvesters_numbers.py
@@ -1,10 +1,10 @@
 s_nums_dict = {}
 s_nums_dict[0] = 2
-def sylversters_number_seq(n, s_nums_dict):
+def sylvesters_number_seq(n, s_nums_dict):
     if (n in s_nums_dict.keys()):
         return s_nums_dict[n]
     else:
-        curr_num = sylversters_number_seq(n-1, s_nums_dict) ** 2 - sylversters_number_seq(n-1, s_nums_dict) + 1
+        curr_num = sylvesters_number_seq(n-1, s_nums_dict) ** 2 - sylvesters_number_seq(n-1, s_nums_dict) + 1
 
         s_nums_dict[n] = curr_num
         return curr_num
@@ -13,5 +13,5 @@ def sylversters_number_seq(n, s_nums_dict):
 count_nums = int(input('Input : N = '))
 print ('Output : ')
 for n in range(0, count_nums):
-    sylversters_number_seq(n, s_nums_dict)
+    sylvesters_number_seq(n, s_nums_dict)
 print(' '.join([str(num) for num in s_nums_dict.values()]))
